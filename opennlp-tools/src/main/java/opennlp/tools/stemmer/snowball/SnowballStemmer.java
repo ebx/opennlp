39df2cd0a90eb45b99964ce42632f18ba1b6e0f4
Maxime Steinmetz <max.steinn@gmail.com>
2019-01-04 13:17:15 +0100

    TEC-6651 Adding greek stemmer

--- opennlp-tools/src/main/java/opennlp/tools/stemmer/snowball/SnowballStemmer.java
+++ opennlp-tools/src/main/java/opennlp/tools/stemmer/snowball/SnowballStemmer.java
@@ -28,6 +28,7 @@ public class SnowballStemmer implements Stemmer {
     FINNISH,
     FRENCH,
     GERMAN,
+    GREEK,
     HUNGARIAN,
     IRISH,
     ITALIAN,
@@ -64,6 +65,9 @@ public class SnowballStemmer implements Stemmer {
     }
     else if (ALGORITHM.GERMAN.equals(algorithm)) {
       stemmer = new germanStemmer();
+    }
+    else if (ALGORITHM.GREEK.equals(algorithm)) {
+      stemmer = new greekStemmer();
     }
     else if (ALGORITHM.HUNGARIAN.equals(algorithm)) {
       stemmer = new hungarianStemmer();
